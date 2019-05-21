9659d96752cbdd1e3b89b827815fe5bd98cc0e51
GuHaijun <243297288@qq.com>
2019-05-21 16:55:37 +0800

    #modify 打开 .cirrus.yml 注释部分

--- hot_update_ci/deploy_framework.sh
+++ hot_update_ci/deploy_framework.sh
@@ -14,8 +14,7 @@ cd deploy_repo
 git config user.name "HaiJun Gu"
 git config user.email "243297288@qq.com"
 rm -rf $FRAMEWORK_PATH && mkdir -p $FRAMEWORK_PATH
-cp $ENGINE_PATH/.gclient $FRAMEWORK_PATH
-# cp -Rf target/ios_debug_all/Flutter.framework HotUpdate/1.0.0/iOS/Flutter.framework
+cp -Rf target/ios_debug_all/Flutter.framework $FRAMEWORK_PATH/Flutter.framework
 git add --all .
 git commit -m "Cirrus CI Auto Builder"
 git push --force https://$DEPLOY_TOKEN@github.com/FlutterPrograms/EngineRepo.git master
